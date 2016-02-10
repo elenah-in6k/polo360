6e59514a569162d36f0bb184eea731050a073241
elenah-in6k <elenah@interlink-ua.com>
2016-02-10 09:18:25 +0200

    reformat code

--- js/jcarousel-control.js
+++ js/jcarousel-control.js
@@ -1,13 +1,8 @@
-$(function() {
-  // Инициализация слайдера
-  $('.jcarousel').jcarousel({
-    // Базовые настройки скрипта пишутся здесь
-  });
-
+$(function () {
+    $('.jcarousel').jcarousel({});
     $('.jcarousel-pagination').jcarouselPagination({
-      item: function(page) {
-          return '<a href="#' + page + '">' + page + '</a>';
-      }
-  });
-
+        item: function (page) {
+            return '<a href="#' + page + '">' + page + '</a>';
+        }
+    });
 });
